b03ea67a59a77f089eb3165a7419274619ea1026
Jinx-_-GT <70546159+JinxGT@users.noreply.github.com>
2020-10-21 20:42:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const prefix = "[Your Prefix]"
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag} :)`);
-    bot.user.setActivity("SMEXY BIGBERRY", { // THIS IS THE PLACE YOU PUT YOUR CUSTOM STATUS
+    bot.user.setActivity("BERY BIG SMEXY", { // THIS IS THE PLACE YOU PUT YOUR CUSTOM STATUS
         type: "STREAMING", // THIS IS THE STATUS TYPE
         url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" // THIS IS THE LINK IT WILL BRING YOU TO WHEN YOU CLICK "Watch Stream"!
     });
