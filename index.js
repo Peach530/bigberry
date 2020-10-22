a7c3eb6d906584d846acb4afee8b9417c2ef7333
Jinx-_-GT <70546159+JinxGT@users.noreply.github.com>
2020-10-21 20:40:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const prefix = "[Your Prefix]"
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag} :)`);
-    bot.user.setActivity("[your custom status]", { // THIS IS THE PLACE YOU PUT YOUR CUSTOM STATUS
+    bot.user.setActivity("SMEXY BIGBERRY", { // THIS IS THE PLACE YOU PUT YOUR CUSTOM STATUS
         type: "STREAMING", // THIS IS THE STATUS TYPE
         url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" // THIS IS THE LINK IT WILL BRING YOU TO WHEN YOU CLICK "Watch Stream"!
     });
@@ -15,10 +15,6 @@ bot.on('ready', () => {
 }
 )
 
-// THIS IS THE ([prefix]help) COMMAND
-
-// [SCRIPT GOES HERE]
-
-// THIS IS THE bot.login
+//THIS IS THE COMMANDS
 
 bot.login(process.env.token);
