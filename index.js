b6fb4b79eff0db3a714ede8fc406fbb15b665efe
Peach530 <73361027+Peach530@users.noreply.github.com>
2020-10-23 16:05:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const prefix = "bery!"
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag} :)`);
-    bot.user.setActivity("BERY SMEXY | bery!help", { // THIS IS THE PLACE YOU PUT YOUR CUSTOM STATUS
-        type: "STREAMING", // THIS IS THE STATUS TYPE
-        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" // THIS IS THE LINK IT WILL BRING YOU TO WHEN YOU CLICK "Watch Stream"!
+    bot.user.setActivity("BERY SMEXY | bery!help", { 
+        type: "STREAMING", 
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" 
     });
 
 }
