bfbe36c58215f700dda9e675a74c896b827441ab
Jinx-_-GT <70546159+JinxGT@users.noreply.github.com>
2020-10-23 10:35:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -81,25 +81,6 @@ bot.on("message", async message => {
     }
 });
 
-  if (command === "dog") {
-       
-        const subReddits = ["dog", "doggy", "dogs", "dogloaf"]
-        const random = subReddits[Math.floor(Math.random() * subReddits.length)]
-
-        const img = await randomPuppy(random);
-
-        const embed = new Discord.MessageEmbed()
-            .setColor('RANDOM')
-		.setTitle('json.title')
-		.setAuthor('BigBery', 'https://media.discordapp.net/attachments/768993167357771777/769236103332233226/unknown.png')
-		.setURL(`https://reddit.com/r/${random}`)
-		.setTimestamp()
-		.setFooter('Bendy Development', 'https://images-ext-1.discordapp.net/external/iLWEn8WA-KTd-w9KlU6s4HdLW9PPzZb-16IO0ML3tnA/%3Fwidth%3D294%26height%3D294/https/media.discordapp.net/attachments/750544950860447764/756916779791679599/bendy_regular.jpg');
-
-        return message.channel.send(embed)
-            .catch(err => message.reply(`Something went wrong... ${err}`));
-    }
-});
 //THIS IS THE COMMANDS
 
 bot.login(process.env.token);
